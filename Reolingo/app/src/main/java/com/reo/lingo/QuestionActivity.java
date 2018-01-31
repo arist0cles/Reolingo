4b605ed1d152742f8a2f2c15f12395167e8c7d2d
patrick ryan <pattyboy_07@hotmail.com>
2018-01-31 22:51:28 +1300

    Added main activity and question activity

--- Reolingo/app/src/main/java/com/reo/lingo/QuestionActivity.java
+++ Reolingo/app/src/main/java/com/reo/lingo/QuestionActivity.java
@@ -1,12 +1,8 @@
 package com.reo.lingo;
 
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AlertDialog;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -16,24 +12,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity
+public class QuestionActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
-    private int studyTime;
-    private Button fiveMinButton;
-    private Button tenMinButton;
-    private Button fifthteenMinButton;
-    private Button twentyMinButton;
-    private MainActivity main;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_question);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -54,63 +40,6 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-
-        main = this;
-        setupButtons();
-
-    }
-
-    public void setupButtons(){
-        Button fiveMinButton = (Button) this.findViewById(R.id.fivemins);
-        Button tenMinButton = (Button) this.findViewById(R.id.tenmins);
-        Button fifthteenMinButton = (Button) this.findViewById(R.id.fifthteenmins);
-        Button twentyMinButton = (Button) this.findViewById(R.id.twentymins);
-
-        fiveMinButton.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v) {
-                //play success sound
-                //popup dialog
-                //on dialog close start asking questions
-                studyTimeChosen(5);
-            }
-        });
-
-        tenMinButton.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v) {
-                studyTimeChosen(10);
-            }
-        });
-
-        fifthteenMinButton.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v) {
-                studyTimeChosen(15);
-            }
-        });
-
-        twentyMinButton.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v) {
-                studyTimeChosen(20);
-            }
-        });
-    }
-
-    public void studyTimeChosen(int time){
-        final MediaPlayer mp = MediaPlayer.create(this, R.raw.beep);
-        mp.start();
-        studyTime = time;
-
-        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-        builder.setMessage("Ka Pai, I'll do my best to help you get to "+studyTime+" minutes a day")
-                .setTitle("Chur")
-                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener(){
-                            public void onClick(DialogInterface i, int j){
-                                Intent intent = new Intent(main, QuestionActivity.class);
-                                startActivity(intent);
-                            }
-                        }
-                );
-        AlertDialog dialog = builder.create();
-        dialog.show();
     }
 
     @Override
@@ -123,12 +52,10 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.question, menu);
         return true;
     }
 
