7a67e873e7a007521cc2ca742c3918cb624ae020
Wa <wwainohu@gmail.com>
2018-02-11 17:51:47 +1300

    Work in progress: on Firebase push notifications

---
+++ Reolingo/app/src/main/java/com/reo/lingo/InstanceID.java
@@ -0,0 +1,11 @@
+package com.reo.lingo;
+
+/**
+ * Created by Warena on 11/02/2018.
+ */
+
+class InstanceID {
+}
+
+
+
---
+++ Reolingo/app/src/main/java/com/reo/lingo/RegistrationService.java
@@ -0,0 +1,28 @@
+//package com.reo.lingo;
+//
+//import android.app.IntentService;
+//import android.content.Intent;
+//import android.os.Build;
+//import android.support.annotation.Nullable;
+//import android.support.annotation.RequiresApi;
+//
+///**
+// * Created by Warena on 11/02/2018.
+// */
+//
+//@RequiresApi(api = Build.VERSION_CODES.CUPCAKE)
+//public class RegistrationService extends FirebaseInstanceIDService {
+//    /**
+//     * Creates an IntentService.  Invoked by your subclass's constructor.
+//     *
+//     * @param name Used to name the worker thread, important only for debugging.
+//     */
+//    public RegistrationService(String name) {
+//        super(name);
+//    }
+//
+//   // @Override
+//    protected void onHandleIntent(@Nullable Intent intent) {
+//        InstanceID myID = InstanceID.getInstance(this);
+//    }
+//}
